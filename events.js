c9673a6ec00011792bec46a5c5deda90069cfa8f
차트박스 <chartbox91@gmail.com>
2025-11-06 02:42:33 +0900

    Update events.js

--- events.js
+++ events.js
@@ -5,11 +5,6 @@ window.EVENTS = [
     start: "2025-11-05",
     url: "https://www.notion.so/25-11-5-2a2312e0f9a4809ba1d2d4d31983794c"
   },
-  {
-    title: "시황공유",
-    start: "2025-11-06",
-    url: "https://www.notion.so/25-11-5-2a2312e0f9a4809ba1d2d4d31983794c"
-  },
   // 예시 추가 방법:
   // { title: "시황공유", start: "2025-11-06", url: "https://www.notion.so/..." },
 ];
